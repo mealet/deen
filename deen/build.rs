105457253d8e6c522d81be0134084ccb087d9971
mealet <jasonragonter@gmail.com>
2025-06-01 16:26:58 +0300

    deen \ v0.0.4 \ fixed static hash in version

--- deen/build.rs
+++ deen/build.rs
@@ -8,5 +8,7 @@ fn main() {
         .unwrap();
 
     let git_hash = String::from_utf8(git_output.stdout).unwrap();
+
+    println!("cargo:rerun-if-changed=.git/HEAD");
     println!("cargo:rustc-env=GIT_HASH={}", git_hash);
 }
